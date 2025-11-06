dd12643e41055d698ea11978a9b3276cb42b7c05
Sean Monahan <seanm@donedesigninc.com>
2025-11-06 16:56:54 -0500

    oil buffer reloads with <leader>r

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,24 @@ return {
     'stevearc/oil.nvim',
     dependencies = { "echasnovski/mini.icons" },
     config = function()
-        require("oil").setup()
+        local oil = require("oil")
+        oil.setup()
+
+        -- Open Oil with <leader>fe
         vim.keymap.set("n", "<leader>fe", ":Oil<CR>", { noremap = true, silent = true })
-    end
+
+        -- In Oil buffers, make <leader>r refresh
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "oil",
+            callback = function(event)
+                local actions = require("oil.actions")
+                vim.keymap.set(
+                    "n",
+                    "<leader>r",
+                    actions.refresh.callback,
+                    { buffer = event.buf, silent = true, desc = "Refresh Oil" }
+                )
+            end,
+        })
+    end,
 }
