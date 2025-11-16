972aa66a1e9e7ce413542cbe348778ffd3df389f
Sean Monahan <sean.monahan.ee@gmail.com>
2025-11-16 13:14:50 -0500

    updated lualine with cleaner component separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,25 @@
 return {
     'nvim-lualine/lualine.nvim',
-    event = "VeryLazy", -- Load after startup
+    event = "VeryLazy",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         local lualine = require('lualine')
         lualine.setup({
             options = {
-                icons_enabled = true,
-                theme = "auto",
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                disabled_filetypes = {
+                icons_enabled        = true,
+                theme                = "auto",
+
+                -- Powerline-style separators
+                component_separators = { left = '', right = '' },
+                section_separators   = { left = '', right = '' },
+
+                disabled_filetypes   = {
                     statusline = { "nerdtree" },
                     winbar = { "nerdtree" },
                 },
-                ignore_focus = {},
                 always_divide_middle = true,
-                globalstatus = false,
-                refresh = {
+                globalstatus         = false,
+                refresh              = {
                     statusline = 1000,
                     tabline = 1000,
                     winbar = 1000,
@@ -38,11 +40,7 @@ return {
                 lualine_x = { 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
+            }
         })
     end
 }
