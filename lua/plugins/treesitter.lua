3683f455169ac97668f5268e5e1080d5d8061d8f
Sean Monahan <sean.monahan.ee@gmail.com>
2025-12-23 11:03:17 -0500

    fixed compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,13 +4,20 @@ return {
         event = { "BufReadPost", "BufNewFile" }, -- Load when opening files
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.config").setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "html", "css", "javascript", "mermaid", "yaml", "markdown" },
-                auto_install = true,
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
+            local ok, ts = pcall(require, "nvim-treesitter.configs")
+            if not ok then
+                ok, ts = pcall(require, "nvim-treesitter.config")
+            end
+
+            if ok then
+                ts.setup({
+                    ensure_installed = { "c", "lua", "vim", "vimdoc", "html", "css", "javascript", "mermaid", "yaml", "markdown" },
+                    auto_install = true,
+                    sync_install = false,
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
+            end
         end
     }
 }
