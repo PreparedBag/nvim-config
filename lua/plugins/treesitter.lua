fb1fd58f26be96b4823ab308b5ccd4b146acaa54
Sean Monahan <seanm@donedesigninc.com>
2026-01-09 11:10:55 -0500

    fix attempt on treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        event = { "BufReadPost", "BufNewFile" }, -- Load when opening files
+        lazy = false,
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
