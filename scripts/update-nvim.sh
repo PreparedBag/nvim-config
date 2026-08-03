4762eb42b8b26e917a4435d2af78255669e71e85
Sean Monahan <seanm@donedesigninc.com>
2026-08-03 18:47:52 -0400

    updated nvim version to 0.12.4

--- scripts/update-nvim.sh
+++ scripts/update-nvim.sh
@@ -4,8 +4,9 @@
 # Bump NVIM_VERSION deliberately after testing - this never chases "latest"
 # automatically, so every machine gets the same build until you move the pin.
 
-# NVIM_VERSION="v0.12.4"
-NVIM_VERSION="v0.11.7"
+#  TODO: update version
+NVIM_VERSION="v0.12.4"
+# NVIM_VERSION="v0.11.7"
 
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; NC='\033[0m'
 info()    { echo -e "${BLUE}[INFO]${NC} $1"; }
