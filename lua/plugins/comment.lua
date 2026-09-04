d5e6a10af3e99ded519ca78b2af6f07dfac5d492
Sean Monahan <seanm@donedesigninc.com>
2026-09-04 09:41:09 -0400

    added todo-comment reference in file for easier adjustments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -17,6 +17,24 @@ return {
         end
     },
     {
+        --[[ keywords = {
+        FIX = {
+          icon = " ", -- icon used for the sign, and in search results
+          color = "error", -- can be a hex color, or a named color (see below)
+          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+          -- signs = false, -- configure signs for some keywords individually
+        },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      },
+      gui_style = {
+        fg = "NONE", -- The gui style to use for the fg highlight group.
+        bg = "BOLD", -- The gui style to use for the bg highlight group.
+      }, ]]
         "folke/todo-comments.nvim",
         enabled = require("config.flags").get("LSP_ENABLED"),
         dependencies = { "nvim-lua/plenary.nvim" },
